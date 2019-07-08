2a5bc33afc0081d8f0b3bb28bb3d004e60c1cf96
Sergei <staroduber@yandex.ru>
2019-07-08 22:49:10 +0400

    lesson2 hooks

---
+++ lesson/lesson1.js
@@ -0,0 +1,27 @@
+const root = new Vue({
+  el: ".root",
+  data: {
+  
+  },
+  methods: {
+    
+  },
+  beforeCreate() {
+    
+  },
+  created() {
+    
+  },
+  beforeMount() {
+    
+  },
+  mounted() {
+    
+  },
+  beforeUpdate() {
+    
+  },
+  updated() {
+    
+  },
+})
