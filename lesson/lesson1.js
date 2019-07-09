b314978c59f84c3a50ad0a3981a19ad3aca576d1
Sergei <staroduber@yandex.ru>
2019-07-09 21:52:04 +0400

    add layout for validation form

--- lesson/lesson1.js
+++ lesson/lesson1.js
@@ -1,27 +1,40 @@
 const root = new Vue({
   el: ".root",
   data: {
-  
+    name: "",
+    some: "hello",
+    properties : {
+      color: "",
+      backgroundColor: "" 
+    }
   },
   methods: {
-    
+    show() {
+      console.log("hello");
+
+    }
+  },
+  computed: {
+    className() {
+      return "alert-" + this.some
+    }
   },
   beforeCreate() {
-    
+
   },
   created() {
-    
+
   },
   beforeMount() {
-    
+
   },
   mounted() {
-    
+
   },
   beforeUpdate() {
-    
+
   },
   updated() {
-    
+
   },
 })
